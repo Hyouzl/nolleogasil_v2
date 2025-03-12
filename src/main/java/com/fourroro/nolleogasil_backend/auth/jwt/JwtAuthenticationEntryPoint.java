5ab2fffcb0d09eef8bd475f5cc816ab628be1602
hyouzl <hyouzl0521@gmail.com>
2025-03-13 02:36:03 +0900

    Refactor: spring security 자체 자격증명에러 전역 예외로 처리 (#7)

--- src/main/java/com/fourroro/nolleogasil_backend/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/fourroro/nolleogasil_backend/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,19 +1,28 @@
 package com.fourroro.nolleogasil_backend.auth.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        Map<String, Object> body = new HashMap<>();
+        body.put("code", 401);
+        body.put("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+        response.getWriter().write(objectMapper.writeValueAsString(body));
     }
 }
